83f2852a7ee5025d412a6c64062a878a0233f789
Daniela Rodrigues <daniela_rodrigues@live.com.pt>
2023-05-25 19:22:03 +0100

    colocar unidades dentro de util EnumTexts

--- src/app/utils/pipes/enum_texts.ts
+++ src/app/utils/pipes/enum_texts.ts
@@ -11,4 +11,23 @@ export class EnumTexts {
         return "Muito Difícil";
     }
   }
+
+  getMeasurementUnitText(unit: number | undefined): string {
+    switch(unit) {
+      case 0: return "Gramas";
+      case 1: return "Quilogramas";
+      case 2: return "Mililitros";
+      case 3: return "Centilitros";
+      case 4: return "Decilitros";
+      case 5: return "Litros";
+      case 6: return "Chávena de Chá";
+      case 7: return "Colher de Café";
+      case 8: return "Colher de Chá";
+      case 9: return "Unidade(s)";
+      case 10: return "Lata(s)";
+      case 11:
+      default:
+        return "Pacote(s)";
+    }
+  }
 }
