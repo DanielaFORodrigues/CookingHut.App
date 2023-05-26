4429dc9a2fa339c2b7c419fd7b85180118b93ad0
Daniela Rodrigues <daniela_rodrigues@live.com.pt>
2023-05-26 18:18:47 +0100

    add userfavoritesComponent

---
+++ src/app/components/backoffice/userFavorites/userFavorites.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userFavorites',
+  templateUrl: './userFavorites.component.html',
+  styleUrls: ['./userFavorites.component.css']
+})
+export class UserFavoritesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
