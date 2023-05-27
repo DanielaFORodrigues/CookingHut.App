8c458eedd8955f74885ba25389abbf765622ddc9
Daniela Rodrigues <daniela_rodrigues@live.com.pt>
2023-05-27 17:37:15 +0100

    melhorias listagem receitas

--- src/app/components/backoffice/listRecipes/listRecipes.component.ts
+++ src/app/components/backoffice/listRecipes/listRecipes.component.ts
@@ -5,6 +5,7 @@ import { Category } from 'src/app/models/category';
 import { Recipe } from 'src/app/models/recipe';
 import { CategoryService } from 'src/app/services/category.service';
 import { RecipeService } from 'src/app/services/recipe.service';
+import { UserContextService } from 'src/app/utils/contexts/usercontext.service';
 import { EnumTexts } from 'src/app/utils/pipes/enum_texts';
 
 @Component({
@@ -22,6 +23,7 @@ export class ListRecipesComponent implements OnInit {
   category!: Category | null;
 
   constructor(
+    private userContext: UserContextService,
     private recipeService: RecipeService,
     private categoryService: CategoryService,
     private activatedRoute: ActivatedRoute,
@@ -31,6 +33,15 @@ export class ListRecipesComponent implements OnInit {
   ngOnInit() {
     if (this.pageType === "category") {
       this.loadCategoryPage();
+    }
+    else if(this.pageType === "owner") {
+      this.loadOwnRecipes();
+    }
+    else if(this.pageType === "favourites") {
+      //falta ter metodo aqui
+    }
+    else {
+      this.router.navigate(['home']);
     }
   }
 
@@ -46,6 +57,26 @@ export class ListRecipesComponent implements OnInit {
 
       this.recipeService.getAll("category", categoryId).subscribe(response => {
         this.recipes = response;
+
+        this.pageTitle = `Receitas de ${this.category!.name}`
+      },
+      (error: HttpErrorResponse) => {
+        this.router.navigate(['home']);
+      });
+    });
+  }
+
+  loadOwnRecipes() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      const session = this.userContext.getCurrentSession();
+
+      if (session == null) {
+        this.router.navigate(['home']);
+      }
+
+
+      this.recipeService.getAll("owner", session!.id).subscribe(response => {
+        this.recipes = response;
       },
       (error: HttpErrorResponse) => {
         this.router.navigate(['home']);
@@ -60,8 +91,6 @@ export class ListRecipesComponent implements OnInit {
   getCategory(categoryId: number) {
     this.categoryService.getById(categoryId).subscribe(response => {
       this.category = response;
-
-      this.pageTitle = `Receitas de ${this.category.name}`
     },
     (error: HttpErrorResponse) => {
       this.router.navigate(['home']);
